8671430d924ed93c1affe0971acc01f8db833de4
evelyn masso <coding@outofambit.com>
2023-11-14 15:56:46 -0800

    run code in iframe

--- src/main.js
+++ src/main.js
@@ -6,6 +6,31 @@ let view = new EditorView({
   parent: document.body,
 });
 
-export default function () {
+// taken from https://github.com/codemirror/website/blob/master/site/try/try.ts
+function run() {
+  const currentIframe = document.querySelector("iframe");
+  if (currentIframe) currentIframe.remove();
+  console.log("running");
+  let frame = document.createElement("iframe");
+  frame.setAttribute(
+    "sandbox",
+    "allow-scripts allow-popups allow-modals allow-forms"
+  );
+  frame.src = "sandbox.html";
+  let code = view.state.doc.toString();
+  frame.onload = () => {
+    frame.contentWindow.postMessage({ type: "load", code: code }, "*");
+  };
+  document.body.appendChild(frame);
+  return true;
+}
+
+function setup() {
+  window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    document.querySelector("#run").addEventListener("click", run);
+  });
   console.log("hi");
 }
+
+setup();
