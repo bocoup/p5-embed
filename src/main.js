d7be1500302fad214a43a66639f2815068e1dfac
evelyn masso <coding@outofambit.com>
2023-11-14 16:08:58 -0800

    include starter sketch

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ let view = new EditorView({
   extensions: [basicSetup, javascript()],
   parent: document.body,
 });
+view.dispatch({
+  changes: {
+    from: 0,
+    insert: `
+    let sketch = function (p) {
+      let x = 100;
+      let y = 100;
+
+      p.setup = function () {
+        p.createCanvas(700, 410);
+      };
+
+      p.draw = function () {
+        p.background(0);
+        p.fill(255);
+        p.rect(x, y, 50, 50);
+      };
+    };
+
+    let myp5 = new p5(sketch);
+    `,
+  },
+});
 
 // taken from https://github.com/codemirror/website/blob/master/site/try/try.ts
 function run() {
