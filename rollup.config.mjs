bbaa82e49b1055ae95a7da3bc55acb36ccdf5821
evelyn masso <coding@outofambit.com>
2023-12-04 17:57:33 -0800

    add jsx file and build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
@@ -10,5 +11,23 @@ export default {
     file: "out/bundle.js",
     format: "es",
   },
-  plugins: [nodeResolve(), commonjs(), cssbundle(), terser()],
+  plugins: [
+    nodeResolve({ extensions: [".js", ".jsx"] }),
+    babel({
+      babelHelpers: "bundled",
+      plugins: [
+        [
+          "@babel/plugin-transform-react-jsx",
+          {
+            pragma: "h",
+            pragmaFrag: "Fragment",
+          },
+        ],
+      ],
+      extensions: [".js", ".jsx"],
+    }),
+    commonjs(),
+    cssbundle(),
+    terser(),
+  ],
 };
---
+++ src/component.jsx
@@ -0,0 +1,5 @@
+import { h } from "preact";
+
+export const CodeEmbed = (props) => {
+  return <p>this is a code embed</p>;
+};
